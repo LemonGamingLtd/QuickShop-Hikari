dc56e8980a34aa413634f78a04247a43c2258c4f
Nahuel Dolores <contact@nahu.me>
2024-10-16 13:55:22 +0200

    Add compatibility implementation for Oraxen

---
+++ compatibility/oraxen/src/main/java/com/ghostchu/quickshop/compatibility/oraxen/Main.java
@@ -0,0 +1,40 @@
+package com.ghostchu.quickshop.compatibility.oraxen;
+
+import com.ghostchu.quickshop.QuickShop;
+import com.ghostchu.quickshop.api.registry.BuiltInRegistry;
+import com.ghostchu.quickshop.api.registry.Registry;
+import com.ghostchu.quickshop.api.registry.builtin.itemexpression.ItemExpressionHandler;
+import com.ghostchu.quickshop.api.registry.builtin.itemexpression.ItemExpressionRegistry;
+import com.ghostchu.quickshop.compatibility.CompatibilityModule;
+import io.th0rgal.oraxen.api.OraxenItems;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+public final class Main extends CompatibilityModule implements ItemExpressionHandler {
+
+  @Override
+  public void init() {
+    final Registry registry = QuickShop.getInstance().getRegistry().getRegistry(BuiltInRegistry.ITEM_EXPRESSION);
+    if(registry instanceof ItemExpressionRegistry itemExpressionRegistry) {
+      if(itemExpressionRegistry.registerHandlerSafely(this)) {
+        getLogger().info("Register Oraxen ItemExpressionHandler successfully!");
+      }
+    }
+  }
+
+  @Override
+  public @NotNull Plugin getPlugin() {
+    return this;
+  }
+
+  @Override
+  public String getPrefix() {
+    return "oraxen";
+  }
+
+  @Override
+  public boolean match(final ItemStack stack, final String expression) {
+    return expression.equals(OraxenItems.getIdByItem(stack));
+  }
+}
