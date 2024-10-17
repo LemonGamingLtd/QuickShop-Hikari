d127815273189553d575aa66b857131c3cc07d17
Nahuel Dolores <contact@nahu.me>
2024-10-17 12:23:22 +0200

    Make registry fetch display names

--- compatibility/oraxen/src/main/java/com/ghostchu/quickshop/compatibility/oraxen/Main.java
+++ compatibility/oraxen/src/main/java/com/ghostchu/quickshop/compatibility/oraxen/Main.java
@@ -7,9 +7,13 @@ import com.ghostchu.quickshop.api.registry.builtin.itemexpression.ItemExpression
 import com.ghostchu.quickshop.api.registry.builtin.itemexpression.ItemExpressionRegistry;
 import com.ghostchu.quickshop.compatibility.CompatibilityModule;
 import io.th0rgal.oraxen.api.OraxenItems;
+import io.th0rgal.oraxen.items.ItemBuilder;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public final class Main extends CompatibilityModule implements ItemExpressionHandler {
 
@@ -36,5 +40,16 @@ public final class Main extends CompatibilityModule implements ItemExpressionHan
   @Override
   public boolean match(final ItemStack stack, final String expression) {
     return expression.equals(OraxenItems.getIdByItem(stack));
+  }
+
+  @Override
+  public @Nullable Component displayName(@NotNull ItemStack stack) {
+    final ItemBuilder builder = OraxenItems.getBuilderByItem(stack);
+    if (builder == null) {
+      return ItemExpressionHandler.super.displayName(stack);
+    }
+
+    final String displayName = builder.getItemName();
+    return displayName != null ? MiniMessage.miniMessage().deserialize(displayName) : ItemExpressionHandler.super.displayName(stack);
   }
 }
